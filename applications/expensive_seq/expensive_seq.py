b1f81819a81506749be4242f22530ad0dfeb9cda
Michael Rowland <mrowland13@gmail.com>
2020-07-09 14:06:38 -0700

    finished expensive_seq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,14 @@
-# Your code here
-
-
+cache = {}
 def expensive_seq(x, y, z):
-    # Your code here
-
+    # use recursion until BASE CASE of x <= 0
+    if x<=0:
+        return y + z
+    # otherwise, check if in cache, and add if missing
+    if (x,y,z) not in cache:
+        cache[(x,y,z)] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+    # return cached item
+    return cache[(x,y,z)]
+    
 
 
 if __name__ == "__main__":
